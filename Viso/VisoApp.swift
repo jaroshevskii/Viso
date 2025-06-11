ac6ced6a882695a1082c7c19a1f40560fe30a31b
Sasha Jaroshevskii <jaroshevskii@icloud.com>
2025-06-11 23:38:11 +0300

    Initial Commit

---
+++ Viso/VisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisoApp.swift
+//  Viso
+//
+//  Created by Sasha Jaroshevskii on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct VisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
